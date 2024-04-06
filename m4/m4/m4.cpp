59322bc643e989e6c8cc7d5b6495745f71a01217
Bhawanjeet Kaur <bhawanjeetgill@gmail.com>
2024-04-05 20:34:07 -0400

    Created parseLine function

--- m4/m4/m4.cpp
+++ m4/m4/m4.cpp
@@ -11,3 +11,48 @@
 #include <string.h>
 #include <stdlib.h>
 #pragma warning(disable: 4996)
+struct flightData
+{
+	char source[20] = "";
+	char destination[20] = "";
+	int fare = 0;
+	char airline[20] = "";
+};
+void displayLeastFareDetails(struct flightData variousFlights[], int count);
+int processFlight(char flightName[20], struct flightData variousFlights[], int* totalCount);
+int main(void)
+{
+	return 0;
+}
+/*
+*
+* NAME: parseLine()
+* DESCRIPTION: This function takes exactly four parameters and will return a status indicating if the parsing of
+*              the airline result worked perfectly or not. The function returns an integer, if the parsing is done
+*              successfully then return 0 else return -1.
+* PARAMETERS: char* source, char* destination, int* fare, char* line
+* RETURNS: 0 or -1
+*/
+
+int parseLine(char* source, char* destination, int* fare, char* line)
+{
+	char* dashPos = strchr(line, '-');
+	if (dashPos != NULL) {
+		*dashPos = '\0'; // Replace '-' with null terminator to separate source
+		strcpy(source, line);
+
+		char* commaPos = strchr(dashPos + 1, ',');
+		if (commaPos != NULL) {
+			*commaPos = '\0'; // Replace ',' with null terminator to separate destination
+			strcpy(destination, dashPos + 1);
+
+			*fare = atoi(commaPos + 1); // Convert fare string to integer
+			return 0; // Parsing successful
+		}
+	}
+	else
+	{
+		printf("Boo!");
+		return -1; // Parsing failed
+	}
+}
