99befc168d3cb799c46625da00b36c15e488a90f
Bhawanjeet Kaur <bhawanjeetgill@gmail.com>
2024-04-05 20:10:39 -0400

    Created the main function

---
+++ m4/m4/m4.cpp
@@ -0,0 +1,13 @@
+/*
+* FILE : m4.cpp
+* PROJECT : SENG1000- major assignment 4
+* PROGRAMMER : Bhawanjeet Kaur Gill
+* FIRST VERSION : 2024-03-27
+* DESCRIPTION : Write a program to display flight information where the least fare found for given source –
+*               destination pair.
+*/
+#include <stdio.h>
+#include <iostream>
+#include <string.h>
+#include <stdlib.h>
+#pragma warning(disable: 4996)
